112486f32108799445cd112e5e693ab47c75f5df
Andrea Odell <andreaodell@D-10-19-248-61.dhcp4.washington.edu>
2018-05-08 14:22:12 -0700

    Created a histogram of length vs scalelength

--- scripts/fish-analysis.R
+++ scripts/fish-analysis.R
@@ -1,6 +1,14 @@
 fish_data <- read.csv("data/Gaeta_etal_CLC_data_1.csv")
+library(ggplot2)
+View(fish_data_cat)
 
 ## creates new table categorizing fish by size 
 fish_data_cat <-  fish_data %>%
   mutate(length_cat = ifelse(length > 300, "big", "small")) %>% 
   filter(scalelength > 1)
+
+
+ggplot(data = fish_data_cat, mapping = aes(length, scalelength, color = lakeid)) +
+  geom_point()
+
+  
