85cbb4617a857d5721d46cae98377b3dc627ec94
Andrea Odell <andreaodell@D-10-19-248-61.dhcp4.washington.edu>
2018-05-08 13:53:23 -0700

    removed fish with scale length less than 1mm

--- scripts/fish-analysis.R
+++ scripts/fish-analysis.R
@@ -3,3 +3,5 @@ fish_data <- read.csv("data/Gaeta_etal_CLC_data.csv")
 ## creates new table categorizing fish by size 
 fish_data_cat <-  fish_data %>%
   mutate(length_cat = ifelse(length > 300, "big", "small"))
+
+
